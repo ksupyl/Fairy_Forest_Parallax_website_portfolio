46f9680e1aa4952ffbe2b2be985fb1b6821bd717
Ksenia <Xeonica01owl@gmail.com>
2024-07-15 15:47:17 +0300

    Commit 8 - logic of moving Parallax layers in .css and add changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 window.addEventListener('scroll', e => {
-    document.body.style.cssText = `scrollTop: ${this.scrollY}px`
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`);
 })
