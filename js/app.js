ab05b7f99aaa73552bbf04bdcd0d53bbed16f1cb
Ksenia <Xeonica01owl@gmail.com>
2024-07-16 15:44:51 +0300

    Commit 13 - smooth website's scroll with JavaScript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 window.addEventListener('scroll', e => {
 	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`);
 })
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content',
+})
